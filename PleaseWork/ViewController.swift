e8bd72a4f837dac77184ede5210163df9e6bf885
Hazel Egan <hazelegan@hazels-mac.local>
2016-03-03 13:23:36 -0800

    Initial Commit

---
+++ PleaseWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PleaseWork
+//
+//  Created by Hazel Egan on 03/03/2016.
+//  Copyright © 2016 Hazel Egan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
